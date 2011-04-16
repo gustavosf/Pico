f4195abc86f5d71b417f3da38f5e6b0fbcf23a76
Gustavo Seganfredo <gustavosf@gmail.com>
2011-04-15 21:36:51 -0300

    Primeira versão do PICO (Etapa 1) - Adicionados Lexemas - Adicionados Tokens

--- src/tokens.h
+++ src/tokens.h
@@ -3,24 +3,54 @@
  * A escolha dos valores constantes eh totalmente arbitraria.
  * Cada valor deve ser distinto...
  */
-#define INT 100
-#define DOUBLE 101
-#define FLOAT 102
-#define CHAR 103
-#define QUOTE 104
-#define DQUOTE 105
-#define LE 106
-#define GE 107
-#define EQ 108
-#define NE 109
-#define AND 110
-#define OR 111
-#define NOT 112
-#define IF 113
-#define THEN 114
-#define ELSE 115
-#define WHILE 116
 
-#define IDF 200
-#define INT_LIT 201
-#define F_LIT 202
+#define INT         1100
+#define DOUBLE      1101
+#define REAL        1102
+#define CHAR        1103
+/*
+#define ASTERISK    1104
+#define PLUS        1105
+#define MINUS       1106
+#define SLASH       1107
+#define COMMA       1108
+#define SEMICOLON   1109
+#define COLON       1110
+*/
+#define QUOTE       1111
+#define DQUOTE      1112
+/*
+#define PARENTHESIS 1113
+#define BRACKETS    1114
+#define GTLT        1115
+#define ATTRIB      1116
+*/
+#define LE          1117
+#define GE          1118
+#define EQ          1119
+#define NE          1120
+#define AND         1121
+#define OR          1122
+#define NOT         1123
+#define IF          1124
+#define THEN        1125
+#define ELSE        1126
+#define FOR         1127
+#define NEXT        1128
+#define WHILE       1129
+#define REPEAT      1130
+#define UNTIL       1131
+#define END         1132
+#define CASE        1133
+#define TRUE        1134
+#define FALSE       1135
+
+/*
+#define SPACE       2200
+*/
+#define IDF         2201
+#define CONST       2202
+#define INT_LIT     2203
+#define F_LIT       2204
+
+#define COMMENT     3000
